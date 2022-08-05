41f6286b3730167c99f7fc9bc76cf141e54854f8
CHOISEOWOO <47114075+choiseowoo@users.noreply.github.com>
2022-08-05 15:54:58 +0900

    Create ListClientExample.java

---
+++ Structures/ListClientExample.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class ListClientExample{
+    public List list;
+    
+    public ListClientExample() {
+        list = new LinkedList();
+    }
+    
+    private List getList() {
+        return list;
+    }
+    
+    public static void main(String[] args) {
+        ListClientExample lce = new ListClientExample();
+        List list = lce.getList();
+        System.out.println(list);
+    }
+    
+}
